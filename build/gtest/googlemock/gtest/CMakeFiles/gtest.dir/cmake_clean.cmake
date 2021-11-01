e69341ef4ec26046ca47b8c321080530a808b207
Rishabh Mukund <rishabh.m96@gmail.com>
2021-11-01 13:25:01 -0400

    After tutorial 2, rosmake

---
+++ build/gtest/googlemock/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  "/home/rishabh-mukund/catkin_ws/src/beginner_tutorials/lib/libgtest.pdb"
+  "/home/rishabh-mukund/catkin_ws/src/beginner_tutorials/lib/libgtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/gtest/googlemock/gtest/CMakeFiles/gtest_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtest_main.dir/src/gtest_main.cc.o"
+  "/home/rishabh-mukund/catkin_ws/src/beginner_tutorials/lib/libgtest_main.pdb"
+  "/home/rishabh-mukund/catkin_ws/src/beginner_tutorials/lib/libgtest_main.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
